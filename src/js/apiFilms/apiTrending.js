7575f1bd88b03cff3d83bc540f3c61ee9c296153
Viktoria Bayer <bayer.skaya@gmail.com>
2022-12-10 22:19:22 +0100

    оно хоть так даст запушить????

--- src/js/apiFilms/apiTrending.js
+++ src/js/apiFilms/apiTrending.js
@@ -5,16 +5,12 @@ import { BASE_URL } from "./baseUrl";
 export class TrendingFilmsApiService{
     constructor(){
         this.page = 1;
-<<<<<<< HEAD
         this.genres = this.fetchGenres();
-=======
->>>>>>> 73ba0387242b39979eb9e6ffe8dca3ae55083b29
     }
 
     async fetchFilms() {
         try {
-<<<<<<< HEAD
-            const options = {params: {api_key: API_KEY}}
+       const options = {params: {api_key: API_KEY}}
             const url = `${BASE_URL}/trending/movie/week?`
           const response = await axios.get(url, options);
           return response.data;
@@ -23,7 +19,7 @@ export class TrendingFilmsApiService{
         }
       }
 
-    async fetchGenres(){
+      async fetchGenres(){
         try {
             const url = `${BASE_URL}/genre/movie/list?api_key=${API_KEY}`
             const gentesList = await axios.get(url)
@@ -32,14 +28,5 @@ export class TrendingFilmsApiService{
             throw new Error(`Oops, something went wrong`)
         }
     }
-=======
-            const options = {params:{"api_key": API_KEY}}
-            const url = `${BASE_URL}/trending/movie/week?`
-          const response = await axios.get('/user?ID=12345', options);
-          console.log(response);
-        } catch (error) {
-          console.error(error);
-        }
-      }
->>>>>>> 73ba0387242b39979eb9e6ffe8dca3ae55083b29
+
 }

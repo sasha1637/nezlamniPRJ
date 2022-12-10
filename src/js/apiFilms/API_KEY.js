0c2efacb687f449419544360be7f8a145e1135d5
Volodymyr <vovdata@gmail.com>
2022-12-10 20:28:41 +0100

    Revert "Merge pull request #4 from Vladimir-Plotnikov/feature/main-page-container"
    This reverts commit 73ba0387242b39979eb9e6ffe8dca3ae55083b29, reversing
    changes made to 1f603ed159a8263520579413d897e2d57579db99.

--- src/js/apiFilms/API_KEY.js
+++
@@ -1,2 +0,0 @@
-
-export const API_KEY = 'cbb822aa772537b57d5d040000698149'
--- src/js/main.js
+++
@@ -1,4 +0,0 @@
-import { TrendingFilmsApiService } from "./apiFilms/apiTrending";
-import axios from 'axios';
-const trendingFilmsApiService = new TrendingFilmsApiService()
-trendingFilmsApiService.fetchFilms().then(response => console.log(response))

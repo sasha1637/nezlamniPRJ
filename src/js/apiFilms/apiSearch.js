bc04f966a4b75ed4843102fc875e2ec123622f56
sasha1637 <82914142+sasha1637@users.noreply.github.com>
2022-12-16 15:50:39 +0200

    123

--- src/js/apiFilms/apiSearch.js
+++ src/js/apiFilms/apiSearch.js
@@ -8,13 +8,17 @@ import infiniteScroll from '../infiniteScroll';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import axios from 'axios';
 
+const container = document.querySelector('.tui-pagination');
+
+
 const searchForm = document.querySelector('.js-search__form');
 const searchInput = document.querySelector('.search_input');
 const filmGallery = document.querySelector('.film-gallery');
 
 searchForm.addEventListener('submit', async (e, page = 1, per_page = 20) => {
   e.preventDefault();
-
+  container.classList.add('hide')
+  console.log(container.classList.add('hide'));
   if (!searchInput.value) {
     return;
   }

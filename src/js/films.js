c6a88cf0864f1f5dd4b00f5686dbd17c833bed16
sasha1637 <82914142+sasha1637@users.noreply.github.com>
2022-12-16 10:44:21 +0200

    Update films.js

--- src/js/films.js
+++ src/js/films.js
@@ -35,38 +35,13 @@ const options = {
 const filmGallery = document.querySelector('.film-gallery');
 const pagination = new Pagination(container, options);
 
+
 async function filmer() {
   try {
     const films = await trending.fetchFilms();
     const genres = await trending.fetchGenres();
     const total_results = films.total_results;
-    const options = {
-      totalItems: `${total_results}`,
-      itemsPerPage: 20,
-      visiblePages: 10,
-      page: 1,
-      centerAlign: true,
-      firstItemClassName: 'tui-first-child',
-      lastItemClassName: 'tui-last-child',
-      template: {
-        page: '<a href="#" class="tui-page-btn">{{page}}</a>',
-        currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
-        moveButton:
-          '<a href="#" class="tui-page-btn tui-{{type}}">' +
-            '<span class="tui-ico-{{type}}">{{type}}</span>' +
-          '</a>',
-        disabledMoveButton:
-          '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
-            '<span class="tui-ico-{{type}}">{{type}}</span>' +
-          '</span>',
-        moreButton:
-          '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
-            '<span class="tui-ico-ellip">...</span>' +
-          '</a>'
-      }
-    };
-    const pagination = new Pagination(container, options);
-
+console.log(total_results);
     trending.genres = genres;
     filmGallery.innerHTML = markUpGallery(films.results,genres)
     filmGallery.insertAdjacentHTML('beforeend', markUpGallery(films.results, genres));
